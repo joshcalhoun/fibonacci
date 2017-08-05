2d8086ea93394477dbd0b46269367ab77c74308f
sturg13 <sturgilldawson@gmail.com>
2017-08-05 16:24:55 -0400

    Added contacts

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from core import *
+#from core import *
 while True:
-    request = input('Hello, what would you like to do?')
-    grant(request)
+    #request = input('Hello, what would you like to do?')
+    #grant(request)
+    request = input("Whats up")
+    if (request == "nothing"):
+        print ("do somthing then")
 
---
+++ contacts.py
@@ -0,0 +1,15 @@
+contacts = {
+	'Dawson':['610-716-7668','sturgilldawson@gmail.com'],
+	'Chad':  ['561-329-9821','sturg6@comcast.net'],
+	'Beth':  ['610-716-7668','sturg4@comcast.net']}
+while True:
+    request = input("Which contact do you want?")
+    second_request = input("Phone or Email?")
+    if (second_request.lower() == "phone"):
+        print(contacts[str(request)][0])
+    elif (second_request.lower() == "email"):
+        print(contacts[str(request)][1])
+    option = input("Quit or continue")
+    if (option == "quit"):
+        break
+    
